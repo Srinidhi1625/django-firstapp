ddb42b9ccd011cb130b48c94e03bfdfc0eaddbe7
Srinidhi1625 <nidhisri16@gmail.com>
2022-02-19 06:09:09 +0530

    version1

---
+++ firstproject/firstApp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def wish(request):
+    return HttpResponse('<h1>Good Morning</h1><p>This is Nidhi!!! She is a good girl</p>')
