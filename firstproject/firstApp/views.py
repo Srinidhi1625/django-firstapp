1d6c43c636200463535c5868afe6063585f64d49
Srinidhi1625 <nidhisri16@gmail.com>
2022-02-19 06:18:58 +0530

    version2

--- firstproject/firstApp/views.py
+++ firstproject/firstApp/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def wish(request):
-    return HttpResponse('<h1>Good Morning</h1><p>This is Nidhi!!! She is a good girl</p>')
+    return HttpResponse('<h1>Good Morning</h1><p>This is Nidhi!!! She is a good girl!!! :)</p>')
